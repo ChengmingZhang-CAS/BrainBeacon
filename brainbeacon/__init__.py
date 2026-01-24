067cbfc5abf5fc80c8c579774c98a632bdbaae29
ChengmingZhang-CAS <zhangchengming2017@sibcb.ac.cn>
2026-01-25 01:19:54 +0900

    Add API reference page

--- brainbeacon/__init__.py
+++ brainbeacon/__init__.py
@@ -1,8 +1,15 @@
 from importlib.metadata import PackageNotFoundError, version
 
+from .pipeline.cell_embedding import run_bbcellformer_pipeline
+from .pipeline.cell_label_transfer import run_label_transfer_pipeline
+
+__all__ = [
+    "__version__",
+    "run_bbcellformer_pipeline",
+    "run_label_transfer_pipeline",
+]
+
 try:
     __version__ = version("brainbeacon")
 except PackageNotFoundError:
-    __version__ = "dev"
-
-__all__ = ["__version__"]
+    __version__ = "0.0.1"
