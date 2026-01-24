54a43fc3850b3f7119db43b6c4258aca90cb9918
ChengmingZhang-CAS <zhangchengming2017@sibcb.ac.cn>
2026-01-25 01:29:30 +0900

    Fix docs failing bug

--- brainbeacon/__init__.py
+++ brainbeacon/__init__.py
@@ -1,15 +1,8 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from .pipeline.cell_embedding import run_bbcellformer_pipeline
-from .pipeline.cell_label_transfer import run_label_transfer_pipeline
-
-__all__ = [
-    "__version__",
-    "run_bbcellformer_pipeline",
-    "run_label_transfer_pipeline",
-]
-
 try:
     __version__ = version("brainbeacon")
 except PackageNotFoundError:
     __version__ = "0.0.1"
+
+__all__ = ["__version__"]
--- brainbeacon/pipeline/__init__.py
+++ brainbeacon/pipeline/__init__.py
@@ -1,2 +1,7 @@
-# brainbeacon/pipeline/__init__.py
-__all__ = []
+from .cell_embedding import run_bbcellformer_pipeline
+from .cell_label_transfer import run_label_transfer_pipeline
+
+__all__ = [
+    "run_bbcellformer_pipeline",
+    "run_label_transfer_pipeline",
+]
