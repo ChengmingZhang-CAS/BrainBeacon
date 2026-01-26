e9b28fe363e412137c68af627f91e05b36f58ced
Yiwen Yang <yangyw27@sjtu.edu.cn>
2026-01-26 07:56:24 +0000

    Update_tutorails

--- brainbeacon/configs/config.py
+++ brainbeacon/configs/config.py
@@ -32,13 +32,12 @@ MAX_LENGTH = 4096
 AUX_TOKEN = 20
 
 DEFAULT_PATHS = {
-    "DATA_PATH": "/cpfs01/projects-HDD/cfff-282dafecea22_HDD/public/BrainST",
-    "BASE_DIR": "/raid/zhangchengming/BrainBeacon-master",
-    "PRETRAIN_DIR": "/raid/zhangchengming/BrainBeacon-master/pretrained",
-    "PRIOR_DIR": "/raid/zhangchengming/BrainBeacon-master/prior_knowledge",
-    "GENE_DICT_PATH": "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/model_h5ad_1211.h5ad",
-    "GENE_LOOKUP_DIR": "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/gene_lookup",
-    "ESM_EMBED_PATH": "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/esm2_embeddings_d5120.pt",
+    "BASE_DIR": "/need_file_path/BrainBeacon-master",
+    "PRETRAIN_DIR": "/need_file_path/BrainBeacon-master/pretrained",
+    "PRIOR_DIR": "/need_file_path/BrainBeacon-master/prior_knowledge",
+    "GENE_DICT_PATH": "/rneed_file_path/prior_knowledge/model_h5ad_1211.h5ad",
+    "GENE_LOOKUP_DIR": "/need_file_path/prior_knowledge/gene_lookup",
+    "ESM_EMBED_PATH": "/need_file_path/prior_knowledge/esm2_embeddings_d5120.pt",
 }
 
 def resolve_path(key: str, path_dict: dict | None = None) -> str:
@@ -52,10 +51,3 @@ def resolve_path(key: str, path_dict: dict | None = None) -> str:
         if v is not None and str(v).strip() != "":
             return str(v)
     return DEFAULT_PATHS[key]
-
-# DATA_PATH = '/cpfs01/projects-HDD/cfff-282dafecea22_HDD/public/BrainST'
-# BASE_DIR = "/raid/zhangchengming/BrainBeacon-master"
-# Gene_dict_path = "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/model_h5ad_1211.h5ad"
-# GENE_DICT_PATH = "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/model_h5ad_1211.h5ad"
-# GENE_LOOKUP_DIR = "/raid/zhangchengming/BrainBeacon-master/prior_knowledge/gene_lookup"
-# PRETRAIN_DIR = "/raid/zhangchengming/BrainBeacon-master/pretrained"
